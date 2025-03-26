2c2150ef750af556d64e9f413a459bdd0571059b
Arin Singhal <arinsuniv@gmail.com>
2025-03-26 14:19:56 +0530

    Update Pascal_triangle.py

--- Task1A/Pascal_triangle.py
+++ Task1A/Pascal_triangle.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        tri = [[1]]
+        tri = [[1]] #pascal triangle list
         for i in range(numRows-1):
-            a = [1]
-            b = tri[-1] + [0]
+            a = [1] #first element of row is 1, so started list with that
+            b = tri[-1] + [0] #added 0 at last because the last element of a would have caused problems (below)
             for j in range(1,len(tri[-1])+1):
-                a.append(b[j-1]+b[j])
+                a.append(b[j-1]+b[j]) 
             tri.append(a)
         return tri
 
